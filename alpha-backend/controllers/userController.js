78eda3afaf3711206e452d370e0925ecf42ed543
rizkyark <73096634+rizkyark@users.noreply.github.com>
2021-03-17 00:15:31 +0700

    PPA-1 child: PPA-2, PPA-3, PPA-4, PPA-5

---
+++ alpha-backend/controllers/userController.js
@@ -0,0 +1,18 @@
+const db = require('../database')
+const { createToken } = require('../helpers/jwtHelper')
+const { asyncQuery } = require('../helpers/queryHelper')
+
+module.exports = {
+    login: async (req, res) => {
+        try {
+            const userQuery = await asyncQuery(`SELECT * FROM users WHERE ${db.escape(req.body.username)} IN (username, email) AND password=${db.escape(req.body.password)}`)
+            if (userQuery.length === 0) return res.status(400).send('Invalid username and/or password')
+            let token = createToken({ id: userQuery[0].id_user, username: userQuery[0].username })
+            userQuery[0].token = token
+            res.status(200).send(userQuery[0])
+        } catch (err) {
+            
+        }
+    },
+   
+}
