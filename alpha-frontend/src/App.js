9441147240af5939425596d339acbdbfb8cfc779
bayu <bayuferdiman1@gmail.com>
2021-03-23 13:56:25 +0700

    -update

--- alpha-frontend/src/App.js
+++ alpha-frontend/src/App.js
@@ -6,38 +6,35 @@ import {
   HomePage,
   AccountPage,
   ChangePassPage,
-  ProductDetail,
-  RegisterPage,
-  LoginPage
-  ProfilPage,
-  VerificationPage,
+  ForgetPage,
+  LoginPage,
+  RegisterPage
 } from "./pages";
 import { keepLoginAction } from "./redux/actions";
-
-// link color
-// https://www.happyhues.co/palettes/15
 
 class App extends Component {
   state = {};
   componentDidMount() {
     const { keepLoginAction } = this.props;
     const token = localStorage.getItem("token");
-    if (token) {keepLoginAction()};
+    if (token) {
+      keepLoginAction();
+    }
   }
   render() {
     return (
       <div>
         <Header />
         <Route path="/" exact component={HomePage} />
+        <Route path="/register" component={RegisterPage} />
         <Route path="/account" component={AccountPage} />
         <Route path="/login" component={LoginPage} />
+        <Route path="/forget-password" component={ForgetPage} />
         <Route path="/change-password" component={ChangePassPage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/product/:id" component={ProductDetail} />
-        <Route path="/verify" component={VerificationPage} />
       </div>
     );
   }
 }
+
 
 export default connect(null, {keepLoginAction}) (App);
